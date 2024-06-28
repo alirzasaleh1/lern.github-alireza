0d344d6c83348b1a0683a79d6559f2cbfdd420d4
alirzasaleh1 <alireza.salehi.n.3456@gmail.com>
2024-06-28 04:31:35 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>site</title>
+</head>
+<body bgcolor="#a9a9a9" >
+
+<table align="center" border="4" bordercolor="Black" width="700" height="500">
+    <tr>
+        <td height="200" >
+            <img src= "عکس%20امام.jpg" height="230" width="720" border="2" >
+
+
+
+        </td>
+    <tr>
+        <td border="4" bgcolor="#6495ed" width="300" height="100" >
+            <marquee diraction="alternote" >
+              <b>
+                  <h6>  سایت دانشکده فنی حرفه ای میناب  </h6>
+              </b>
+            </marquee>
+
+
+
+        </td>
+    </tr>
+
+    <tr border="4" bgcolor="#6495ed" >
+        <td>
+            <table width="100" height="200" border="4" >
+                <tr bgcolor="#6495ed" width="400" >
+                    <td> <img src="images.jfif" width="499" height="694" > </td>
+                    <td>
+                        <table border="4" height="695" width="200" >
+                            <tr>
+                                <td><ul align="left" type="circle">
+                                        <li> <a><h6>اسامی استادان</h6> </a> </li>
+                                        <li> <a><h6>ساعات کار اداری</h6></a> </li>
+                                        <li> <a><h6>کارکنان</h6></a> </li>
+                                        <li> <a><h6>توضیحات</h6></a> </li>
+
+
+                                    </ul>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+
+
+
+</body>
+</html>
